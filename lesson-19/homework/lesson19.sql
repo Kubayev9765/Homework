f191ef963ff6756ecf476e21006d9d93283dbcdc
Kubayev9765 <ShamanRamilov@gmail.com>
2025-02-24 11:44:38 +0500

    Update lesson19.sql

--- lesson-19/homework/lesson19.sql
+++ lesson-19/homework/lesson19.sql
@@ -1,1 +1,24 @@
+--CREATE DATABASE Class_3
 
+/* 
+F18- Gruppa KUBAYEV Tolqin Abdalimovich.
+HOMEWORK
+*/
+
+/*1. Barcha sotuvchilarni ko'rsatish.Barcha sotuvchilar haqidagi barcha ma'lumotlarni aks ettiruvchi SQL bayonotini yozing.
+
+1. Display All Salespeople
+*/
+
+--CREATE table salesmen(
+--	salesmen_id bigint,
+--	salesman_name varchar(50),
+--	city varchar(50),
+--	comission decimal
+--)
+
+--select *from salesmen
+
+--insert into salesmen values
+
+ select salesman_name, comission from salesmen
